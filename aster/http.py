847b12417b25083f4386b7338c38ce3d1198c080
BenitzCoding <benitzcoding@yahoo.com>
2023-03-14 19:32:10 +0800

    Added more HTTP request methods.

--- aster/http.py
+++ aster/http.py
@@ -91,7 +91,7 @@ class HTTPClient:
             dict: The updated data.
         """
         async with aiohttp.ClientSession() as session:
-            async with session.post(
+            async with session.patch(
                 f"{self.url}/{database}/{collection}/update",
                 headers = {
                     "Authorization": self.private_key,
@@ -99,6 +99,127 @@ class HTTPClient:
                 json = {
                     "query": query,
                     "data": data
+                }
+            ) as response:
+                if response.status == 200:
+                    return await response.json().get("result")
+
+                raise RESPONSE_MAP.get(response.status, UnknownError(f"The server encountered an unknown error: HTTP {response.status}"))
+
+    async def delete(self, database: str, collection: str, query: dict) -> dict:
+        """Deletes data in a collection.
+
+        Args:
+            collection (str): The name of the collection.
+            query (dict): The query to find the data.
+
+        Returns:
+            dict: The deleted data.
+        """
+        async with aiohttp.ClientSession() as session:
+            async with session.delete(
+                f"{self.url}/{database}/{collection}/delete",
+                headers = {
+                    "Authorization": self.private_key,
+                },
+                json = {
+                    "query": query
+                }
+            ) as response:
+                if response.status == 200:
+                    return await response.json().get("result")
+
+                raise RESPONSE_MAP.get(response.status, UnknownError(f"The server encountered an unknown error: HTTP {response.status}"))
+
+    async def create_collection(self, database: str, collection: str) -> dict:
+        """Creates a collection.
+
+        Args:
+            collection (str): The name of the collection.
+
+        Returns:
+            dict: The created collection.
+        """
+        async with aiohttp.ClientSession() as session:
+            async with session.create(
+                f"{self.url}/{database}/create",
+                headers = {
+                    "Authorization": self.private_key,
+                },
+                json = {
+                    "collection": collection
+                }
+            ) as response:
+                if response.status == 200:
+                    return await response.json().get("result")
+
+                raise RESPONSE_MAP.get(response.status, UnknownError(f"The server encountered an unknown error: HTTP {response.status}"))
+
+    async def delete_collection(self, database: str, collection: str) -> dict:
+        """Deletes a collection.
+
+        Args:
+            collection (str): The name of the collection.
+
+        Returns:
+            dict: The deleted collection.
+        """
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                f"{self.url}/{database}/delete",
+                headers = {
+                    "Authorization": self.private_key,
+                },
+                json = {
+                    "collection": collection
+                }
+            ) as response:
+                if response.status == 200:
+                    return await response.json().get("result")
+
+                raise RESPONSE_MAP.get(response.status, UnknownError(f"The server encountered an unknown error: HTTP {response.status}"))
+
+    async def create_database(self, database: str) -> dict:
+        """Creates a database.
+
+        Args:
+            database (str): The name of the database.
+
+        Returns:
+            dict: The created database.
+        """
+        async with aiohttp.ClientSession() as session:
+            async with session.create(
+                f"{self.url}/create",
+                headers = {
+                    "Authorization": self.private_key,
+                },
+                json = {
+                    "database": database
+                }
+            ) as response:
+                if response.status == 200:
+                    return await response.json().get("result")
+
+                raise RESPONSE_MAP.get(response.status, UnknownError(f"The server encountered an unknown error: HTTP {response.status}"))
+            
+    async def delete_database(self, database: str) -> dict:
+        """Deletes a database.
+
+        Args:
+            database (str): The name of the database.
+
+        Returns:
+            dict: The deleted database.
+        """
+        async with aiohttp.ClientSession() as session:
+            async with session.delete(
+                f"{self.url}/delete",
+                headers = {
+                    "Authorization": self.private_key,
+                },
+                json = {
+                    "database": database
                 }
             ) as response:
                 if response.status == 200:
